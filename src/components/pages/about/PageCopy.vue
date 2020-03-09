2e2341d6e0176da2587ef20493f49890537f69c5
John Bauer <johnbauer126@gmail.com>
2020-03-09 16:41:22 -0400

    update backlog

--- src/components/pages/about/PageCopy.vue
+++ src/components/pages/about/PageCopy.vue
@@ -20,6 +20,27 @@
       so this is an attempt to present the same data in some different and
       potentially useful ways.
     </p>
+    <SectionTitle title="Backlog" />
+    <p class="subtitle is-size-7-touch">
+      This project is still in-progress. Here are some
+      features/defects/modifications on the horizon:
+    </p>
+    <div class="content">
+      <ul type="1">
+        <li>
+          Multiline chart for historical changes in cases, confirmed, deaths.
+          (global)
+        </li>
+        <li>Implement Mapbox GL with hotspots by location</li>
+        <li>Add resources page to study, CDC info, etc.</li>
+        <li>Refactor snapshots</li>
+        <li>
+          Write util functions for getting countries, counting confirmed,
+          recovered, deaths, etc.
+        </li>
+      </ul>
+    </div>
+    <br />
     <SectionTitle title="Contribution & Use" />
     <p class="subtitle is-size-7-touch">
       If you have any suggestions, spot a defect, or see improvements that can
