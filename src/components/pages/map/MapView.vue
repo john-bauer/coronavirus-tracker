6213b265d18e0812853addcc3c9d200780001551
John Bauer <johnbauer126@gmail.com>
2020-03-12 12:41:36 -0400

    successful first go at mapping

--- src/components/pages/map/MapView.vue
+++ src/components/pages/map/MapView.vue
@@ -1,55 +1,103 @@
 <template>
-  <div>
-    <pre>
-      {{ coronavirusData }}
-    </pre>
-    <MglMap
-      @load="onMapLoaded"
-      container="map"
-      :accessToken="accessToken"
-      :mapStyle="mapStyle"
-      class="-map"
-    >
-      <MglMarker :coordinates="coordinates" :color="color">
-        <MglPopup>
-          <VCard>
-            <div>Hello, I'm popup!</div>
-          </VCard>
-        </MglPopup>
-      </MglMarker>
-    </MglMap>
-  </div>
+  <div id="map" class="-map"></div>
 </template>
 
 <script>
-import { MglMap, MglMarker, MglPopup } from "vue-mapbox";
-import { mapState } from "vuex";
+import mapboxgl from "mapbox-gl";
+import GeoJSON from "geojson";
+import { mapState, mapActions } from "vuex";
 
 export default {
-  name: "MapSandbox",
-  data() {
+  name: "MapViewVanilla",
+  data: function() {
     return {
-      accessToken: process.env.VUE_APP_MAPBOXACCESSTOKEN,
-      mapStyle: "mapbox://styles/mapbox/dark-v9",
-      coordinates: [-111.549668, 39.014],
-      color: "red"
+      map: null,
+      locations: null,
+      sampleData: [
+        {
+          name: "Location A",
+          category: "Store",
+          street: "Market",
+          lat: 39.984,
+          lng: -75.343
+        },
+        {
+          name: "Location B",
+          category: "House",
+          street: "Broad",
+          lat: 39.284,
+          lng: -75.833
+        },
+        {
+          name: "Location C",
+          category: "Office",
+          street: "South",
+          lat: 39.123,
+          lng: -74.534
+        }
+      ]
     };
   },
   computed: {
     ...mapState("coronavirus", ["coronavirusData"])
   },
   methods: {
-    onMapLoaded(e) {
-      this.map = e.map;
+    ...mapActions("coronavirus", ["getAllCoronavirusData"]),
+    convertCoronavirusData() {
+      let dataSet = [];
+      for (
+        let i = 0;
+        i < this.coronavirusData.confirmed.locations.length;
+        i++
+      ) {
+        dataSet.push({
+          country: this.coronavirusData.confirmed.locations[i].country,
+          province: this.coronavirusData.confirmed.locations[i].province,
+          lat: parseInt(
+            this.coronavirusData.confirmed.locations[i].coordinates.lat
+          ),
+          lng: parseInt(
+            this.coronavirusData.confirmed.locations[i].coordinates.long
+          )
+        });
+      }
+      return dataSet;
     }
   },
-  created() {
-    this.map = null;
+  beforeMount() {
+    this.getAllCoronavirusData();
   },
-  components: {
-    MglMap,
-    MglMarker,
-    MglPopup
+  mounted() {
+    mapboxgl.accessToken = process.env.VUE_APP_MAPBOXACCESSTOKEN;
+    this.map = new mapboxgl.Map({
+      container: "map",
+      style: "mapbox://styles/mapbox/dark-v9",
+      zoom: 2
+    });
+    var map = this.map;
+
+    map.on("load", () => {
+      console.log(GeoJSON.parse(this.sampleData, { Point: ["lat", "lng"] }));
+      map.addControl(new mapboxgl.FullscreenControl());
+      let myData = GeoJSON.parse(this.convertCoronavirusData(), {
+        Point: ["lat", "lng"]
+      });
+      console.log(myData);
+      map.addSource("point", {
+        type: "geojson",
+        data: myData
+      });
+      map.addLayer({
+        id: "point",
+        type: "circle",
+        source: "point",
+        paint: {
+          "circle-radius": 10,
+          "circle-color": "#3887be",
+          "circle-opacity": 0.25
+        }
+      });
+    });
   }
 };
 </script>
@@ -57,6 +105,6 @@ export default {
 <style lang="scss">
 .-map {
   width: 100%;
-  height: 800px;
+  height: 700px;
 }
 </style>
